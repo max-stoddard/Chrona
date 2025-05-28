f6849ad199c200de26fc9da7b9c11f75b8fbd34b
VeerV11 <veervishalvij@gmail.com>
2025-05-28 21:28:36 +0100

    feat[VV]: intialise exam details page skeleton

---
+++ frontend/src/Exams.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+/**
+ * Exams – renders an empty white canvas that fills the viewport.
+ */
+const Exams: React.FC = () => <h1>Exams page works</h1>;
+
+export default Exams;
