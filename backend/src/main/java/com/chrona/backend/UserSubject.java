8f5121a1ab1f80d03c2154a3408908527697dd9e
VeerV11 <veervishalvij@gmail.com>
2025-06-03 14:29:44 +0100

    feat[VV]: implement UserSubject Class

---
+++ backend/src/main/java/com/chrona/backend/UserSubject.java
@@ -0,0 +1,40 @@
+package com.chrona.backend;
+
+import java.util.UUID;
+
+import io.micrometer.common.lang.NonNull;
+
+public class UserSubject {
+
+    private UUID subject_id;
+    private UUID user_id;
+    
+    @NonNull
+    private String subject_name;
+
+    public UUID getSubjectID() {
+        return subject_id;
+    }
+
+    public UUID getUserID() {
+        return user_id;
+    }
+
+    public String subject_name() {
+        return subject_name;
+    }
+
+    public void set_subject_ID(UUID id) {
+        this.subject_id = id;
+    }
+
+    public void set_user_id(UUID id) {
+        this.user_id = id;
+    }
+
+    public void set_subject_name(String name) {
+        this.subject_name = name;
+    } 
+
+
+}
