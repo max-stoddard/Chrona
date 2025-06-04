1a725c93c1d5bab48d12e9444b50928c5360df74
Max Stoddard <ms4623@ic.ac.uk>
2025-06-04 02:41:23 +0100

    refactor [MS]: Refactor navbar with generics

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,42 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../assets/Chrona.png";
-import avatar from "../assets/avatar.png";
-import "../styles/navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/Chrona.png';
+import avatar from '../assets/avatar.png';
+import '../styles/navbar.css';
 
-const Navbar: React.FC = () => {
-  return (
-    <div className="navbar-depth-frame-2">
-      <div className="navbar-depth-frame-wrapper">
-        <div className="navbar-depth-frame-3">
-          <img
-            className="navbar-logo"
-            alt="Chrona logo"
-            src={logo}
-          />
-          <div className="navbar-div-wrapper">
-            <div className="navbar-text-wrapper">Chrona</div>
-          </div>
-        </div>
-      </div>
+const Navbar: React.FC = () => (
+  <div className="navbar">
+    <Link to="/" className="navbar-brand">
+      <img className="navbar-logo" src={logo} alt="Chrona logo" />
+      <span className="navbar-text">Chrona</span>
+    </Link>
 
-      <div className="navbar-depth-frame-4">
-        <div className="navbar-depth-frame-5">
-          <div className="navbar-depth-frame-6" />
-
-          <div className="navbar-depth-frame-7">
-            <Link to="/" className="navbar-link">Dashboard</Link>
-          </div>
-
-          <div className="navbar-depth-frame-7">
-            <Link to="/subjects" className="navbar-link">Subjects</Link>
-          </div>
-        </div>
-
+    <div className="navbar-links">
+      <Link to="/" className="navbar-link">
+        Dashboard
+      </Link>
+      <Link to="/subjects" className="navbar-link">
+        Subjects
+      </Link>
+      <Link to="/profile">
         <img className="navbar-avatar" src={avatar} alt="avatar" />
-      </div>
+      </Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
