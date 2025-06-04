f6f8b9f7a79c6897a7a0024c076782b7afbed6e2
Max Stoddard <ms4623@ic.ac.uk>
2025-06-04 04:58:01 +0100

    feat [MS]: Create new subject card component

---
+++ frontend/src/components/SubjectCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from './Card';
+import '../styles/typography.css';
+import '../styles/theme.css';
+import '../styles/subjectcard.css';
+
+interface SubjectCardProps {
+  name: string;
+  onView?: () => void;
+}
+
+/**
+ * Renders a single subject as a Card.
+ * "Exams:" section intentionally left blank for now.
+ */
+const SubjectCard: React.FC<SubjectCardProps> = ({ name, onView }) => (
+  <Card className="subject-card">
+    {/* Subject Name */}
+    <h2 className="heading-2 subject-title">{name}</h2>
+    
+    {/* Brief Exams */}
+    <p className="body-2 exams-line">Exams:&nbsp;</p>
+    
+    {/* View Details */}
+    <button className="view-details-button button-text" onClick={onView}>
+      View Details
+    </button>
+  </Card>
+);
+
+export default SubjectCard;
