5ff3f929f8670bb5dfd4ea9423566913d7f6479c
Max Stoddard <ms4623@ic.ac.uk>
2025-06-04 03:47:34 +0100

    fix [MS]: Remove unnecessary imports in subject dao get test

--- backend/src/test/java/com/chrona/backend/db/UserSubjectDaoIntegrationTest.java
+++ backend/src/test/java/com/chrona/backend/db/UserSubjectDaoIntegrationTest.java
@@ -1,22 +1,17 @@
 package com.chrona.backend.db;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+import com.chrona.backend.db.dataAccess.UserSubjectDao;
+import com.chrona.backend.db.models.UserSubject;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
