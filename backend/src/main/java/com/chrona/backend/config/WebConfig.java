1da4153c23afd15278657e6cf691487405297a9b
Max Stoddard <ms4623@ic.ac.uk>
2025-06-04 04:58:44 +0100

    fix [MS]: Fix web config to use CORS config

---
+++ backend/src/main/java/com/chrona/backend/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.chrona.backend.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(cors -> cors.configurationSource(CorsConfig.corsConfigurationSource()))
+      .authorizeHttpRequests(a -> a
+          .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+          .anyRequest().permitAll()
+      )
+      .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    return http.build();
+  }
+
+}
