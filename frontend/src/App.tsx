21654173e65f519d6d68b2c137d96e79e5fe08ef
AizereShaikenova <aizere14.sh@gmail.com>
2025-06-05 15:07:26 +0100

    feat [AS]: Add routing link for SubjectDetails page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard";
 import SubjectsPage from "./pages/Subjects";
 import AddSubjectPage from './pages/AddSubject';
+import SubjectDetailsPage from './pages/SubjectDetailsPage';
+
 
 const App = () => (
 <Routes>
   <Route path="/" element={<Dashboard />} />
   <Route path="/subjects" element={<SubjectsPage />} />
   <Route path="/add-subject" element={<AddSubjectPage />} />
+  <Route path="/subjects/:subjectId" element={<SubjectDetailsPage />} />
 </Routes>
 
 );
