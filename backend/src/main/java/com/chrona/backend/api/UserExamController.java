2a4a2b743808496c8d35f22eb04297f538956e7c
Max Stoddard <ms4623@ic.ac.uk>
2025-06-05 14:17:00 +0100

    feat [MS]: Add controller to get all exams by userId

---
+++ backend/src/main/java/com/chrona/backend/api/UserExamController.java
@@ -0,0 +1,30 @@
+package com.chrona.backend.api;
+
+import com.chrona.backend.db.models.UserSubjectExam;
+import com.chrona.backend.db.dataAccess.SubjectExamDao;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.List;
+
+/**
+ * GET /api/users/{userId}/exams  –  list every exam a user has,
+ * across all of their subjects.
+ */
+@RestController
+@RequestMapping("/api/users/{userId}/exams")
+public class UserExamController {
+
+    private final SubjectExamDao dao;
+
+    public UserExamController(SubjectExamDao dao) {
+        this.dao = dao;
+    }
+
+    @GetMapping
+    public List<UserSubjectExam> list(@PathVariable UUID userId) {
+        return dao.selectAllByUser(userId);
+    }
+}
