8de24a59c97843beb6ca48cf89aa206d5a784452
Max Stoddard <ms4623@ic.ac.uk>
2025-06-05 16:23:04 +0100

    feat [MS]: Implement a page for not found pages

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import '../styles/typography.css';
+import '../styles/theme.css';
+import '../styles/notPages.css';        
+
+export default function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <main className="nf-wrapper">
+      <h1 className="heading-1">404 - Page not found</h1>
+      <p  className="body-2">
+        Sorry, we can't find the page you're looking for.
+      </p>
+
+      <button
+        className="button-start"
+        onClick={() => navigate('/')} 
+      >
+        Back to dashboard
+      </button>
+    </main>
+  );
+}
