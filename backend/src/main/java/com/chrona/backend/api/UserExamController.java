a8eb3717dc6adc48e827552a9c8d8360613f4776
Max Stoddard <ms4623@ic.ac.uk>
2025-06-10 14:50:04 +0100

    feat [MS]: Update exam controller for new DB

--- backend/src/main/java/com/chrona/backend/api/UserExamController.java
+++ backend/src/main/java/com/chrona/backend/api/UserExamController.java
@@ -3,7 +3,6 @@ package com.chrona.backend.api;
 import com.chrona.backend.db.models.UserSubjectExam;
 import com.chrona.backend.db.dataAccess.SubjectExamDao;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
