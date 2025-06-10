0c37338f64d84d22a6509332e37802a6840987c9
Max Stoddard <ms4623@ic.ac.uk>
2025-06-10 19:34:29 +0100

    feat [MS]: Update config to support transactions

--- backend/src/main/java/com/chrona/backend/config/WebConfig.java
+++ backend/src/main/java/com/chrona/backend/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableWebSecurity
+@EnableTransactionManagement
 public class WebConfig {
 
   @Bean
